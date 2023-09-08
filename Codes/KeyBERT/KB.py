e1f787e5acc524c51e2a16d7640eec92bb474be7
Ranccat <wogurjo98@gmail.com>
2023-09-08 13:10:29 +0900

    Extracting keyword into csv

--- Codes/KeyBERT/KB.py
+++ Codes/KeyBERT/KB.py
@@ -1,16 +1,38 @@
 from keybert import KeyBERT
+import csv
 
-fin = open("test1.txt", "r")
+# User Parameters
+wordCount = 10  # How many keywords
 
-summary = fin.readlines()
-
-wordCount = 10
-
+# Models and Lists
 kw_model = KeyBERT()
-keywords = kw_model.extract_keywords(summary, top_n=wordCount)
+plots = []
 
-keyList = []
-for keyword in keywords:
-    keyList.append(keyword[0])
+# Reading .csv
+with open("movies_test.csv", 'r') as file:
+    reader = csv.reader(file)
+    rows = list(reader)
+header = rows[0]
+header.append("keywords")
 
-print(keyList)
+count = 0
+for row in rows[1:]:
+    if count % 10 == 0:
+        print("in progress..", end='')
+        print(f'{count}th row read')
+    count += 1
+
+    summary = row[7]
+    keywords = kw_model.extract_keywords(summary, top_n=wordCount)
+    keyList = []
+    for keyword in keywords:
+        keyList.append(keyword[0])
+    newList = ', '.join(map(str, keyList))
+    row.append(newList)
+
+# Writing .csv
+with open('movies_test.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(rows)
+
+print("Keyword extraction complete")
